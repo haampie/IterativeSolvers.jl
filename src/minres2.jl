4379a668b51ff51578367879a0225dbb3d15da0e
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-21 10:25:01 +0200

    Make it an iterator and some further improvements

--- src/minres2.jl
+++ src/minres2.jl
@@ -1,17 +1,47 @@
-export minres_iterable, minres
+export minres2_iterable, minres2
 
-import Base.LinAlg.BLAS.axpy!
+import Base.LinAlg: BLAS.axpy!, givensAlgorithm
 import Base: start, next, done
 
-function rotation(a, b)
-    r = √(a^2 + b^2)
-    a / r, b / r, r
+type Minres2Iterable{matT, vecT, realT}
+    A::matT
+    x::vecT
+
+    # Krylov basis vectors
+    v_prev::vecT
+    v_curr::vecT
+    v_next::vecT
+
+    # W = R * inv(V) is computed using 3-term recurrence
+    w_prev::vecT
+    w_curr::vecT
+    w_next::vecT
+
+    # Vector of size 4, holding the active column of the Hessenberg matrix
+    # rhs is just two active values of the right-hand side.
+    R::vecT
+    rhs::vecT
+
+    # Some Givens rotations
+    c_prev::realT
+    s_prev::realT
+    c_curr::realT
+    s_curr::realT
+
+    # The normalization constant of v_prev
+    prev_norm::realT
+
+    # Bookkeeping
+    maxiter::Int
+    tolerance::realT
+    resnorm::realT
 end
 
-function minres2(A, b; maxiter = 10)
-    n = length(b)
+minres2_iterable(A, b; kwargs...) = minres2_iterable!(zeros(b), A, b; initially_zero = true, kwargs...)
 
-    x = zeros(b)
+function minres2_iterable!(x, A, b; initially_zero = false, tol = sqrt(eps(real(eltype(b)))), maxiter = size(A, 1))
+    T = real(eltype(b))
+
     v_prev = similar(b)
     v_curr = copy(b)
     v_next = similar(b)
@@ -19,85 +49,113 @@ function minres2(A, b; maxiter = 10)
     w_curr = similar(b)
     w_next = similar(b)
 
-    previous_norm = 0.0
+    # For nonzero x's, we must do an MV for the initial residual vec
+    if !initially_zero
+        # Use v_next to store Ax; v_next will soon be overwritten.
+        A_mul_B!(v_next, A, x)
+        axpy!(-one(T), v_next, v_curr)
+    end
 
     # Last column of the R matrix: QR = H where H is 
     # the tridiagonal Lanczos matrix.
-    R = zeros(4)
+    R = zeros(T, 4)
+
+    resnorm = norm(v_curr)
+    reltol = resnorm * tol
 
     # Last two entries of the right-hand side
-    rhs = [norm(v_curr); 0.0]
+    rhs = [resnorm; zero(T)]
 
     # Normalize the first Krylov basis vector
-    v_curr /= rhs[1]
+    scale!(v_curr, inv(resnorm))
 
     # The normalization constant of v_prev (initially zero)
-    prev_norm = 0.0
+    prev_norm = zero(T)
 
     # Givens rotations
-    c_prev, s_prev = 1.0, 0.0
-    c_curr, s_curr = 1.0, 0.0
+    c_prev, s_prev = one(T), zero(T)
+    c_curr, s_curr = one(T), zero(T)
 
-    for i = 1 : maxiter
-        # v_next = A * v_curr - prev_norm * v_prev
-        A_mul_B!(v_next, A, v_curr)
-
-        if i > 1
-            axpy!(-prev_norm, v_prev, v_next)
-        end
-        
-        # Orthogonalize w.r.t. v_curr
-        R[3] = dot(v_curr, v_next)
-        axpy!(-R[3], v_curr, v_next)
-
-        # Normalize
-        R[4] = prev_norm = norm(v_next)
-
-        v_next /= prev_norm
-
-        # Previous 2 rotations
-        if i > 2
-            # Use the fact that T[iter - 2, iter] = 0
-            R[1] = s_prev * R[2]
-            R[2] = c_prev * R[2]
-        end
-
-        if i > 1
-            tmp = -s_curr * R[2] + c_curr * R[3]
-            R[2] = c_curr * R[2] + s_curr * R[3]
-            R[3] = tmp
-        end
-
-        # New rotation
-        c, s, R[3] = rotation(R[3], R[4])
-
-        # Applied to the rhs:
-        rhs[2] = -s * rhs[1]
-        rhs[1] = c * rhs[1]
-
-        # Update W = V * inv(R)
-        copy!(w_next, v_curr)
-        
-        # Could be BLAS-2 rather than BLAS-1
-        if i > 1
-            axpy!(-R[2], w_curr, w_next)
-        end
-
-        if i > 2
-            axpy!(-R[1], w_prev, w_next)
-        end
-        
-        scale!(w_next, inv(R[3]))
-
-        # Update solution x
-        axpy!(rhs[1], w_next, x)
-
-        # Move on: next -> curr, curr -> prev
-        v_prev, v_curr, v_next = v_curr, v_next, v_prev
-        w_prev, w_curr, w_next = w_curr, w_next, w_prev
-        c_prev, s_prev, c_curr, s_curr = c_curr, s_curr, c, s
-
-        R[2] = prev_norm
-        rhs[1] = rhs[2]
-    end
+    Minres2Iterable(
+        A, x,
+        v_prev, v_curr, v_next,
+        w_prev, w_curr, w_next,
+        R, rhs,
+        c_prev, s_prev, c_curr, s_curr, prev_norm,
+        maxiter, reltol, resnorm
+    )
 end
+
+converged(m::Minres2Iterable) = m.resnorm ≤ m.tolerance
+
+start(::Minres2Iterable) = 1
+
+done(m::Minres2Iterable, iteration::Int) = iteration > m.maxiter || converged(m)
+
+function next(m::Minres2Iterable, iteration::Int)
+    # v_next = A * v_curr - prev_norm * v_prev
+    A_mul_B!(m.v_next, m.A, m.v_curr)
+
+    iteration > 1 && axpy!(-m.prev_norm, m.v_prev, m.v_next)
+    
+    # Orthogonalize w.r.t. v_curr
+    m.R[3] = dot(m.v_curr, m.v_next)
+    axpy!(-m.R[3], m.v_curr, m.v_next)
+
+    # Normalize
+    m.R[4] = m.prev_norm = norm(m.v_next)
+    scale!(m.v_next, inv(m.prev_norm))
+
+    # Rotation on R[1] and R[2]. Note that R[1] = 0 initially
+    if iteration > 2
+        m.R[1] = m.s_prev * m.R[2]
+        m.R[2] = m.c_prev * m.R[2]
+    end
+
+    # Rotation on R[2] and R[3]
+    if iteration > 1
+        tmp = -m.s_curr * m.R[2] + m.c_curr * m.R[3]
+        m.R[2] = m.c_curr * m.R[2] + m.s_curr * m.R[3]
+        m.R[3] = tmp
+    end
+
+    # Next rotation
+    c, s, m.R[3] = givensAlgorithm(m.R[3], m.R[4])
+
+    # Apply as well to the right-hand side
+    m.rhs[2] = -s * m.rhs[1]
+    m.rhs[1] = c * m.rhs[1]
+
+    # Update W = V * inv(R). Two axpy's can maybe be one MV.
+    copy!(m.w_next, m.v_curr)
+    iteration > 1 && axpy!(-m.R[2], m.w_curr, m.w_next)
+    iteration > 2 && axpy!(-m.R[1], m.w_prev, m.w_next)
+    scale!(m.w_next, inv(m.R[3]))
+
+    # Update solution x
+    axpy!(m.rhs[1], m.w_next, m.x)
+
+    # Move on: next -> curr, curr -> prev
+    m.v_prev, m.v_curr, m.v_next = m.v_curr, m.v_next, m.v_prev
+    m.w_prev, m.w_curr, m.w_next = m.w_curr, m.w_next, m.w_prev
+    m.c_prev, m.s_prev, m.c_curr, m.s_curr = m.c_curr, m.s_curr, c, s
+    m.rhs[1] = m.rhs[2]
+
+    # Due to symmetry of the tri-diagonal matrix
+    m.R[2] = m.prev_norm
+
+    # The approximate residual is cheaply available
+    m.resnorm = abs(m.rhs[2])
+
+    m.resnorm, iteration + 1
+end
+
+function minres2!(x, A, b; kwargs...)
+    iterable = minres2_iterable!(x, A, b; kwargs...)
+
+    for resnorm = iterable end
+
+    iterable.x, iterable.resnorm
+end
+
+minres2(A, b; kwargs...) = minres2!(zeros(b), A, b; initially_zero = true, kwargs...)
